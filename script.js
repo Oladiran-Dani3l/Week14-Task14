22215d492960930ae9cb14146b9a3b1f89d4983b
Diran <100717659+Oladiran-Dani3l@users.noreply.github.com>
2025-04-26 20:07:43 +0100

    Functionality to record score and track ball

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
+let football = document.getElementById('ball');
+let footballPitch = document.querySelector('.pitch');
+
+let leftGoal = document.querySelector('.left-goal');
+let rightGoal = document.querySelector('.right-goal');
+
+let homeScore = document.getElementById('home');
+let awayScore = document.getElementById('away');
+
+let homeCount = 0, awayCount = 0;
+
+footballPitch.addEventListener('click',(event) => {
+    let pitchRect = footballPitch.getBoundingClientRect();
+
+    let moveBallX = event.clientX - pitchRect.left;
+    let moveBallY = event.clientY - pitchRect.top;
+    
+    let ballSize = football.offsetWidth;
+
+    football.style.top = (moveBallY - ballSize/2) + 'px';
+    football.style.left = (moveBallX - ballSize/2) + 'px';
 
 
+    // RECORD SCORE
+    let leftRect = leftGoal.getBoundingClientRect();
+    let rightRect = rightGoal.getBoundingClientRect();
+    let footballRect = football.getBoundingClientRect();
 
+const homeGoal =
+    footballRect.left < rightRect.right &&
+    footballRect.right > rightRect.left &&
+    footballRect.top < rightRect.bottom &&
+    footballRect.bottom > rightRect.top;
 
+if (homeGoal) {
+    console.log("GOAL for Home!");
+    homeCount++;
+    homeScore.textContent = homeCount;
+}
 
+const awayGoal =
+footballRect.left < leftRect.right &&
+footballRect.right > leftRect.left &&
+footballRect.top < leftRect.bottom &&
+footballRect.bottom > leftRect.top;
 
-
+if (awayGoal) {
+console.log("GOAL for Away!");
+awayCount++;
+awayScore.textContent = awayCount;
+}
+})
 
 
 
